3df0bfd34720f8780d01d622ee25a69fc0cee714
Christian Chiama <christian.chiama@atcult.it>
2018-10-24 17:44:18 +0200

    add template license in file
    add template license in file

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2018 Be Engineering srl
+ *
+ * @author: Christian Chiama
+ * @author: Gabriele Pettinato
+ * 
+ * 
+ * @version: 1.0.0
+ * @since:2018
+ * @see:http://be-engineering.cloud
+ *
+ * 
+ * 
+ */
